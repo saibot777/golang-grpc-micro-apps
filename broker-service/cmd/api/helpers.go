cb4663ad4b7db60e71180b41de4e37503faef93a
saibot777 <stefantrajkovic777@gmail.com>
2022-06-06 02:20:31 +0200

    Add JSOn helper write func

---
+++ broker-service/cmd/api/helpers.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+func (app *Config) readJson(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1048576
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must have only a single JSON value")
+	}
+
+	return nil
+}
