70f291a5b9f8ae2188abb05924f4a79a9d8544bd
saibot777 <stefantrajkovic777@gmail.com>
2022-06-19 13:46:10 +0200

    Add json helpers funcs

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -26,6 +26,38 @@ func (app *Config) readJson(w http.ResponseWriter, r *http.Request, data any) er
 	return nil
 }
 
-func (app *Config) writeJson(w http.ResponseWriter, r *http.Request, data any) error {
+func (app *Config) writeJson(w http.ResponseWriter, status int, data any, headers, ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return panic(err.Error())
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return panic(err.Error())
+	}
+
 	return nil
+}
+
+func (app *Config) errorJson(w http.ResponseWriter, err error, status, ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload JsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJson(w, statusCode, payload)
 }
